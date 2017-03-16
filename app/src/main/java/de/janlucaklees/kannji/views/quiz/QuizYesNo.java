19d39240829803d856a20090413333b566e64805
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-03-16 19:19:20 +0100

    - disabled answer-buttons when kanji ist still loading

--- app/src/main/java/de/janlucaklees/kannji/views/quiz/QuizYesNo.java
+++ app/src/main/java/de/janlucaklees/kannji/views/quiz/QuizYesNo.java
@@ -3,26 +3,19 @@ package de.janlucaklees.kannji.views.quiz;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONException;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import de.janlucaklees.kannji.MainActivity;
 import de.janlucaklees.kannji.R;
 import de.janlucaklees.kannji.datatypes.Kanji;
 import de.janlucaklees.kannji.services.database.WordDBServerConnectorV1;
-import de.janlucaklees.kannji.views.list.KanjiFragment;
-import de.janlucaklees.kannji.views.list.KanjiList;
 
 public class QuizYesNo extends AppCompatActivity {
 
@@ -43,20 +36,20 @@ public class QuizYesNo extends AppCompatActivity {
 		TextView comboView = (TextView) findViewById( R.id.activity_quiz_yes_no_combo );
 		comboView.setText( comboCount );
 
+		_yesButton = (Button) findViewById( R.id.activity_quiz_yes_no_button_yes );
+		_noButton = (Button) findViewById( R.id.activity_quiz_yes_no_button_no );
 
 		new LoadKanjiTask().execute();
 	}
 
 	public void yesButtonClicked( View view ) {
 
-		_yesButton = (Button) findViewById( R.id.activity_quiz_yes_no_button_yes );
-
 		Intent intent = getIntent();
 
-		if( _answerCorrect ) {
+		if ( _answerCorrect ) {
 			// congratz
 			_yesButton.setBackgroundColor( Color.GREEN );
-			intent.putExtra( BUNDLE_KEY_COMBO, intent.getIntExtra( BUNDLE_KEY_COMBO, 0 ) +1 );
+			intent.putExtra( BUNDLE_KEY_COMBO, intent.getIntExtra( BUNDLE_KEY_COMBO, 0 ) + 1 );
 		} else {
 			// fail
 			_yesButton.setBackgroundColor( Color.RED );
@@ -64,19 +57,17 @@ public class QuizYesNo extends AppCompatActivity {
 		}
 
 		finish();
-		startActivity(intent);
+		startActivity( intent );
 	}
 
 	public void noButtonClicked( View view ) {
 
-		_noButton = (Button) findViewById( R.id.activity_quiz_yes_no_button_no );
-
 		Intent intent = getIntent();
 
-		if( !_answerCorrect ) {
+		if ( !_answerCorrect ) {
 			// congratz
 			_noButton.setBackgroundColor( Color.GREEN );
-			intent.putExtra( BUNDLE_KEY_COMBO, intent.getIntExtra( BUNDLE_KEY_COMBO, 0 ) +1 );
+			intent.putExtra( BUNDLE_KEY_COMBO, intent.getIntExtra( BUNDLE_KEY_COMBO, 0 ) + 1 );
 		} else {
 			// fail
 			_noButton.setBackgroundColor( Color.RED );
@@ -84,7 +75,7 @@ public class QuizYesNo extends AppCompatActivity {
 		}
 
 		finish();
-		startActivity(intent);
+		startActivity( intent );
 	}
 
 	private class LoadKanjiTask extends AsyncTask<Void, Void, List<Kanji>> {
@@ -93,7 +84,8 @@ public class QuizYesNo extends AppCompatActivity {
 
 		@Override
 		protected void onPreExecute() {
-
+			_noButton.setEnabled( false );
+			_yesButton.setEnabled( false );
 		}
 
 		@Override
@@ -101,7 +93,7 @@ public class QuizYesNo extends AppCompatActivity {
 			WordDBServerConnectorV1 kanjiLoader = new WordDBServerConnectorV1();
 
 			// Getting JSON from URL
-			List<Kanji> kanjiList = new ArrayList<Kanji>(  );
+			List<Kanji> kanjiList = new ArrayList<Kanji>();
 
 			try {
 				kanjiList.add( kanjiLoader.getRandomKanji() );
@@ -118,7 +110,7 @@ public class QuizYesNo extends AppCompatActivity {
 		protected void onPostExecute( List<Kanji> kanjis ) {
 
 			// if there was an error, display it stop.
-			if( _error != null ) {
+			if ( _error != null ) {
 				displayError();
 				return;
 			}
@@ -131,7 +123,7 @@ public class QuizYesNo extends AppCompatActivity {
 
 			Random rnd = new Random();
 
-			if( rnd.nextBoolean() ) {
+			if ( rnd.nextBoolean() ) {
 
 				List<String> allQuestionReadings = new ArrayList<String>( questionKanji.getKunReadings() );
 				allQuestionReadings.addAll( questionKanji.getOnReadings() );
@@ -148,7 +140,7 @@ public class QuizYesNo extends AppCompatActivity {
 			}
 
 			// decide if answer correct
-			if( questionKanji.getOnReadings().contains( answer ) || questionKanji.getKunReadings().contains( answer ) ) {
+			if ( questionKanji.getOnReadings().contains( answer ) || questionKanji.getKunReadings().contains( answer ) ) {
 				_answerCorrect = true;
 			}
 
@@ -158,6 +150,9 @@ public class QuizYesNo extends AppCompatActivity {
 
 			TextView answerView = (TextView) findViewById( R.id.activity_quiz_yes_no_given_answer );
 			answerView.setText( answer );
+
+			_noButton.setEnabled( true );
+			_yesButton.setEnabled( true );
 		}
 
 		private void displayError() {
