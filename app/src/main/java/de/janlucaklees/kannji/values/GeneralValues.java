5981667d80a5dbde2cf5dfb01dbea7ee85a58f74
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-04-11 20:46:50 +0200

    - updated the api url, now using https and the new subdomain

--- app/src/main/java/de/janlucaklees/kannji/values/GeneralValues.java
+++ app/src/main/java/de/janlucaklees/kannji/values/GeneralValues.java
@@ -1,7 +1,9 @@
 package de.janlucaklees.kannji.values;
 
 public class GeneralValues {
-	public static final String API_URL = "http://217.182.128.241/kannji/api";
+	
+	public static final String API_URL = "https://api.dontusethisinproduction.fail";
 	
 	public static final int DEFAULT_PAGE_SIZE = 24;
+	
 }
