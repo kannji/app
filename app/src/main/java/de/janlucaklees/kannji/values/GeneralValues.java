815ef248b3cb26010aa3a4f8954931f417ec26df
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-08-20 14:27:15 +0200

    Server URL, client-id and -secrete are now set in build-config allowing for different build-variants. One for development and one for production.

--- app/src/main/java/de/janlucaklees/kannji/values/GeneralValues.java
+++ app/src/main/java/de/janlucaklees/kannji/values/GeneralValues.java
@@ -1,9 +1,13 @@
 package de.janlucaklees.kannji.values;
 
+import de.janlucaklees.kannji.BuildConfig;
+
 public class GeneralValues {
 	
-	public static final String API_URL = "https://api.dontusethisinproduction.fail";
+	public static final String API_URL = BuildConfig.API_SERVER_URL;
 	
 	public static final int DEFAULT_PAGE_SIZE = 24;
 	
+	private GeneralValues() {
+	}
 }
